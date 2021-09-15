54052ed16437c7e08c9039627844755552085727
rimvydas1982 <rimwe22@gmail.com>
2021-09-15 12:52:59 +0300

    last commit

--- src/exam-tasks/task-7/Task7.js
+++ src/exam-tasks/task-7/Task7.js
@@ -9,3 +9,5 @@ Because it didn’t get the context.
 Jūsų paaiškinimas:
 
 */
+// Standartine React.js aplikacija, yra sudaryta is daug skirtingo lygio ir sudetingumo komponentu, kur vieni komponentai yra sudedamosios kitu komponetu dalys.Jie saveikauja tarpusavyje perduodami tam tikra informacija-konteksta, kuri suteikia komponentams funkcionaluma,isvaizda ir veikima.Todel React ir nesuprato Joko:)
+//Jeigu kalbeti apie Use Context, jis suteikia galimybe perduoti duomenis ne tiesiogiai per propsus ir tik zemyn, komponetu medyje.Nereikia propsu passinti vienam elementui per kita.
